56d8d5e44621d182e140b3c26ce9edf90f0c0aab
alperen3160 <ilteralperen56@gmail.com>
2024-11-12 20:06:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,1 @@
 loadstring(game:HttpGet(('https://github.com/alperen3160/proxiesHack-TR/raw/refs/heads/main/ProxiesHack%20TR%20deadzone.lua'),true))()
-if game.PlaceId == 3221241066 then
-    Script()
-end
